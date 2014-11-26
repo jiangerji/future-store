ebe954d8df3d12adae7379b5617704b3cb7e2c37
jiangerji <jiangerji@gmail.com>
2014-11-26 16:28:25 +0800

    add option

--- Tags.py
+++ Tags.py
@@ -8,6 +8,9 @@ import time
 import platform
 import MySQLdb
 
+_FAKE = False
+
+
 """
 智能生活    10
     插座 开关 防丢 灯 窗帘 家电 家居 马桶 电饭煲 水杯 路由器 安防 监控 水壶 咖啡 耳机 眼镜 生活
@@ -33,7 +36,7 @@ OTHER   = 99999
 KEYS = {
     LIFE: ["插座","开关","防丢","灯","窗帘","家电","家居","马桶","电饭煲","水杯","路由","安防","监控","水壶","咖啡","耳机","眼镜","生活","手环","电视","手表","腕表","手镯","报警","节能","家中","手腕","遥控器","POS机","净化","打印机","移动电源","名片","戒指","沐浴","加湿器","电动车","牙刷","充电"],
 
-    SPORT: ["耳机","眼镜","头盔","跑步机","xbox","ps","无人","游戏","gopro","运动","娱乐","视频","音乐","高清","手表","腕表","手镯","手环","手势","遥控器","玩耍","相机","影像","光圈","盒子","戒指","飞行","摄像机","相框","意念","绘画","跳舞","机器人","充电宝","气压","光学技术","手机壳","充电","镜头"],
+    SPORT: ["耳机","眼镜","头盔","跑步机","xbox","ps","无人","游戏","gopro","运动","娱乐","视频","音乐","高清","手表","腕表","手镯","手环","手势","遥控器","玩耍","相机","影像","光圈","盒子","戒指","飞行","摄像机","相框","意念","绘画","跳舞","机器人","充电宝","气压","光学技术","手机壳","充电","镜头","旅行","自行车"],
 
     HEALTHY: ["健康","医疗","血糖","血压","心跳","医生","心电","智能水壶","药","温度计","体重","睡眠","净化","肌肤","体温","婴儿","口罩","流感","病毒","加湿器","牙刷","监护器"],
 
@@ -47,7 +50,8 @@ NAMES = {
     SPORT: u"运动娱乐",
     HEALTHY: u"健康医疗",
     KITCHEN: u"智能厨房",
-    SECURITY: u"智能安全"
+    SECURITY: u"智能安全",
+    OTHER: u"其他"
 }
 
 def mark_tags_to_products(quota=1, ignore=-1):
@@ -95,15 +99,15 @@ def _mark_tags_to_products(cursor, quota=1, ignore=-1):
 
         if len(tags) == 0:
             # tags.append(NAMES[LIFE])
+            print product_id, product_title
             tags.append(OTHER)
 
         # print product_id, product_intro, str(tags).encode("gb2312")
-        print "============="
         # db.execute('update products_view set tags=? where product_id=?', (",".join(tags), product_id))
-
-        values = map(lambda x: (x, product_id), tags)
-        print values
-        cursor.executemany('insert into erji_tz_portfolio_tags_xref (tagsid, contentid) values (%s,%s)', values)
+        if not _FAKE:
+            values = map(lambda x: (x, product_id), tags)
+            print product_id, values
+            cursor.executemany('insert into erji_tz_portfolio_tags_xref (tagsid, contentid) values (%s,%s)', values)
         count += 1
         if count >= quota:
             break
@@ -124,8 +128,11 @@ if __name__ == "__main__":
     oldStdout = sys.stdout  
     sys.stdout = logFile
 
-    count = 1
+    count = 10000
     ignore = -1
+
+    if ("--fake" in sys.argv):
+        _FAKE = True
 
     if len(sys.argv) > 1:
         count = abs(int(sys.argv[1]))
